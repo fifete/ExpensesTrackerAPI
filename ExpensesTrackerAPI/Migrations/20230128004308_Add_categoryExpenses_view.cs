3feeebbe47ff15ac39ae060f30e1bcf2a2e43d83
Daniela <danielaFuentesDev@hotmail.com>
2023-01-27 19:45:54 -0500

    feat: create view category_expenses

---
+++ ExpensesTrackerAPI/Migrations/20230128004308_Add_categoryExpenses_view.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpensesTrackerAPI.Migrations
+{
+    public partial class Add_categoryExpenses_view : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE VIEW category_expenses AS
+            SELECT c.id, SUM(e.amount) as SpendingAmount
+            FROM categories c
+            LEFT JOIN expenses e ON c.id = e.category_id
+            GROUP BY c.id;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

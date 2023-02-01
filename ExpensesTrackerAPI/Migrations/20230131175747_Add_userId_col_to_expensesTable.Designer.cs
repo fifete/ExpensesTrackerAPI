5432ca65dd4ecb11f123d132dc9a6ddffc96f451
Daniela <danielaFuentesDev@hotmail.com>
2023-02-01 10:11:02 -0500

    add user column in expenses table

--- ExpensesTrackerAPI/Migrations/20230131175747_Add_userId_col_to_expensesTable.Designer.cs
+++ ExpensesTrackerAPI/Migrations/20230131175747_Add_userId_col_to_expensesTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ExpensesTrackerAPI.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ExpensesTrackerAPI.Migrations
 {
     [DbContext(typeof(ExpensesTrackerContext))]
-    partial class ExpensesTrackerContextModelSnapshot : ModelSnapshot
+    [Migration("20230131175747_Add_userId_col_to_expensesTable")]
+    partial class Add_userId_col_to_expensesTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ExpensesTrackerAPI/Migrations/20230131175747_Add_userId_col_to_expensesTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpensesTrackerAPI.Migrations
+{
+    public partial class Add_userId_col_to_expensesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SpendingAmount",
+                table: "categories_expenses",
+                newName: "spendingamount");
+
+            migrationBuilder.AddColumn<int>(
+                name: "user_id",
+                table: "expenses",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "user_id_temp",
+                table: "expenses",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "user_id",
+                table: "expenses");
+
+            migrationBuilder.DropColumn(
+                name: "user_id_temp",
+                table: "expenses");
+
+            migrationBuilder.RenameColumn(
+                name: "spendingamount",
+                table: "categories_expenses",
+                newName: "SpendingAmount");
+        }
+    }
+}

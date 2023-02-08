a07a4de87077eeac152e0d3f6c60d2dc38613567
Daniela <danielaFuentesDev@hotmail.com>
2023-02-08 10:14:05 -0500

    update categoryExpenses view, category name and expense date is added

---
+++ ExpensesTrackerAPI/Migrations/20230208151143_Update_ExpensesCategoryView.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpensesTrackerAPI.Migrations
+{
+    public partial class Update_ExpensesCategoryView : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE VIEW category_expenses AS
+            SELECT c.id, c.name, e.user_id_temp, e.date, SUM(e.amount) as SpendingAmount
+            FROM categories c
+            LEFT JOIN expenses e ON c.id = e.category_id
+            GROUP BY c.id, e.user_id_temp, e.date, c.name;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
